4af602e5150045e1e4681452f310841eda4d40e4
restuhanputra <restu.hanputraa@gmail.com>
2022-05-29 20:57:19 +0700

    update: challenge02 with for & if else

--- index.js
+++ index.js
@@ -23,19 +23,37 @@ function lakukanLooping(arrPegawai) {
 
       Contoh: ["Aisyah Nirmala", "Mansur Faisal", ...]
   */
-  let hasilLooping = null;
+  let hasilLooping = [];
+
+  for (let index = 0; index < dataYangAkanDilooping.length; index++) {
+    hasilLooping.push(
+      dataYangAkanDilooping[index].namaDepan + " " + dataYangAkanDilooping[index].namaBelakang
+    );
+  }
 
   /*
     TODO 2: Buatlah sebuah variabel bernama "jumlahPria"
       yang berisi jumlah pria dari masing masing pegawai
   */
-  let jumlahPria = null;
+  let jumlahPria = 0;
+
+  for (let index = 0; index < dataYangAkanDilooping.length; index++) {
+    if ( dataYangAkanDilooping[index].jenisKelamin === 'M') {
+      jumlahPria+=1; //jumlahPria = jumlahPria + 1;
+    }
+  }
 
   /*
     TODO 3: Buatlah sebuah variabel bernama "jumlahWanita"
       yang berisi jumlah wanita dari masing masing pegawai
   */
-  let jumlahWanita = null;
+  let jumlahWanita = 0;
+
+  for (let index = 0; index < dataYangAkanDilooping.length; index++) {
+    if ( dataYangAkanDilooping[index].jenisKelamin === 'F') {
+      jumlahWanita+=1; //jumlahWanita = jumlahWanita + 1;
+    }
+  }
 
   /*
     TODO 4: Buatlah sebuah variabel bernama "komentar"
@@ -49,6 +67,14 @@ function lakukanLooping(arrPegawai) {
         "Jumlah Pria dan Wanita berimbang"
   */
   let komentar = null;
+
+  if (jumlahPria > jumlahWanita) {
+    komentar = "Jumlah Pria lebih banyak dari Wanita";
+  } else if (jumlahWanita > jumlahPria) {
+    komentar = "Jumlah Wanita lebih banyak dari Pria";
+  } else {
+    komentar = "Jumlah Pria dan Wanita berimbang";
+  }
 
   // ! JANGAN DIMODIFIKASI
   return {
